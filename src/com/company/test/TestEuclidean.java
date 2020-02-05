a5ebf9e3b8b505de7edfb15d813469a9a7fb6b0f
JIANCHUJUN <yjc381818896@gmail.com>
2020-02-04 19:04:39 -0500

    next step: no hash

--- src/com/company/test/TestEuclidean.java
+++ src/com/company/test/TestEuclidean.java
@@ -2,7 +2,6 @@ package com.company.test;
 
 import com.company.algorithm.Hop;
 import com.company.algorithm.Jocg;
-import com.company.algorithm.Jocg_DB;
 import com.company.element.Graph;
 import com.company.generator.EuclideanGen;
 
@@ -24,57 +23,66 @@ public class TestEuclidean {
     public static void test() throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("EculideanOut.txt"));
         writer.write("");
-        int[] numV = {5000};//,50000};
-        int times = 1;
-        double bottleneck = 5;
+        int[] numV = {10000};//,50000};
+        int times = 2;
+        double bottleneck = 1;
         for(int i:numV){
             double edgeNum = 0;
             double matchNum = 0;
             double timeElapsedHop = 0;
             double timeElapsedJocg = 0;
+            double wasVisited = 0;
+            double notDelete = 0;
+
 
             for(int time = 1; time <= times; time++){
                 System.out.println("|V| = " + 2*i + " bottleneck = " + bottleneck + " " + "time: " + time + "/" + times);
 //                System.out.println("|V| = " + 2*i);
                 EuclideanGen euclideanGen = new EuclideanGen(5);
-                Graph graph = euclideanGen.generate(i,80,20,0.01,bottleneck);
+                //Graph graph = euclideanGen.generate(i,80,20,0.01,bottleneck);
+                Graph graph = euclideanGen.perfectGen(800,100,20,19,2.01);
 
-//                long starth = System.currentTimeMillis();
-//                Hop hop = new Hop(graph);
-//                hop.start();
-//                long finishh = System.currentTimeMillis();
-//                timeElapsedHop += finishh - starth;
-//                int hopMatchCount = graph.matchCount();
-//
-//                assert graph.matchValidCheck();
-//                graph.resetMatch();
-//                //matchingTable_Hop.printMatching();
-//                assert graph.matchCount() == 0;
+                long starth = System.currentTimeMillis();
+                Hop hop = new Hop(graph);
+                hop.start();
+                long finishh = System.currentTimeMillis();
+                timeElapsedHop += finishh - starth;
+                int hopMatchCount = graph.matchCount();
+                assert graph.matchValidCheck();
+
+                graph.resetMatch();
+                assert graph.matchCount() == 0;
 
                 long startj = System.currentTimeMillis();
+                //Jocg_DB jocg = new Jocg_DB(graph);
                 Jocg jocg = new Jocg(graph);
                 jocg.start();
                 long finishj = System.currentTimeMillis();
                 timeElapsedJocg += finishj - startj;
                 int joMatchCount = graph.matchCount();
-
-                System.out.println(jocg.iterate);
-//                System.out.println(hop.iterate);
-//                assert joMatchCount == hopMatchCount;
+                assert joMatchCount == hopMatchCount;
                 assert graph.matchValidCheck();
 
-                //assert hop.iterate >= jocg.iterate;
+                System.out.println(hop.iterate);
+                System.out.println(jocg.iterate);
+
 
                 edgeNum += graph.edgeNum;
                 matchNum += graph.matchCount();
+//                wasVisited += jocg.wasVisited;
+//                notDelete += jocg.notDeleted;
 
             }
             timeElapsedHop /= times;
             timeElapsedJocg /= times;
             edgeNum /= times;
             matchNum /= times;
+            wasVisited /= times;
+            notDelete /= times;
 
             double ratio = timeElapsedJocg/timeElapsedHop;
+            System.out.println("average Visted Edges: " + wasVisited);
+            System.out.println("average not Delete Edges: " + notDelete);
             System.out.println("average Edge number: " + edgeNum);
             System.out.println("average Matching number: " + matchNum);
             System.out.println("average Jocg: " + timeElapsedJocg);
@@ -84,6 +92,8 @@ public class TestEuclidean {
 
 
             writer.append("|V| = " + 2*i + " bottleneck = " + bottleneck + " times = " + times +'\n');
+            writer.append("average Visted Edges: " + wasVisited + "\n");
+            writer.append("average not Delete Edges: " + notDelete + "\n");
             writer.append("average Edge number: " + edgeNum + "\n");
             writer.append("average Matching number: " + matchNum + "\n");
             writer.append("average Jocg: " + timeElapsedJocg + "\n");
