b846fa7acfda526ce76bed7b00ff4072e40cab0f
JIANCHUJUN <yjc381818896@gmail.com>
2020-02-09 15:22:52 -0500

    Add pointer features

--- src/com/company/element/Vertex.java
+++ src/com/company/element/Vertex.java
@@ -2,22 +2,32 @@ package com.company.element;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Vertex {
     public int id;
     public Label label;
-    public ArrayList<Vertex> edges;
+    public LinkedList<Vertex> edges;
+    /*
+     * visitedE is the index for the last vertex(edge) in visitedE
+     * tempE is the index for the last vertex(edge) in tempE
+     * onezero is the the index for the last edge(vertex) with weight 1 in edges
+     */
+    private int visitedE;
+    private int tempE;
+    private int onezero;
+    private int current;
+
     public Vertex matching;
     public Graph piece;
+
     public boolean explored;
-    public HashSet<Vertex> visitedE;
-    public HashSet<Vertex> tempE;
     public int distance;
 
     public Vertex(int id, Label label){
         this.id = id;
         this.label = label;
-        this.edges = new ArrayList<>();
+        this.edges = new LinkedList<>();
         this.matching = null;
         this.piece = null;
         this.explored = false;
