fc1c089ba2cb47beddd401814b5e796e050038f1
Ye <yjc381818896@gmail.com>
2020-03-27 14:08:23 -0400

    3-26 Arc-Expe_Ver

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,10 @@ import com.company.test.TestEdgeMaker;
 import com.company.test.TestEuclidean;
 import com.company.test.TestPerformance;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,8 +16,37 @@ public class Main {
 	    // write your code here
         //TestEuclidean.test();
         //TestPerformance.test1();
-        FindBottle.find();
-
+        FindBottle findBottle = new FindBottle();
+        findBottle.find();
         //TestEdgeMaker.test();
+
+//        File file = new File("TestManager");
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("TestManagerOut"));
+//        writer.write("");
+//        //System.out.println(file.getAbsolutePath());
+//        Scanner scanner = new Scanner(file);
+//        String head = scanner.nextLine();
+//        if(head == null){
+//            writer.write("Wrong Head!\n");
+//            writer.close();
+//            return;
+//        }
+//        HashMap<String, String> param = new HashMap<>();
+//
+//        String line = scanner.nextLine();
+//        while (line != null){
+//            String[] lineArr = line.split("");
+//            param.put(lineArr[0],lineArr[1]);
+//            line = scanner.nextLine();
+//        }
+//
+//        if(head.equals("findBottle")){
+//            FindBottle findBottle = new FindBottle();
+//            findBottle.resetParam(param);
+//            findBottle.find();
+//        }else if(head.equals("graphMatching")){
+//
+//        }
+
     }
 }
